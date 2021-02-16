c25b7b808e9185348c59e8fa0b5f4a1a715992ac
Will Dey <will_dey@college.harvard.edu>
2021-02-16 16:25:12 -0600

    New changes based on work on Ultra-Quantum Matter site

--- js/poker.js
+++ js/poker.js
@@ -1,1 +1,13 @@
 // JS SPECIFIC TO THIS CUSTOM THEME GOES HERE
+if (window.location.pathname.split("/") < 3) {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+	var items = xhr.responseXML.querySelectorAll("item");
+	var slides = document.querySelectorAll("ul.rslides > li");
+	for (var i = 0; i < slides.length; i++) {
+	    if (items[i]) {
+		items[i].querySelector("description");
+	    }
+	}
+    };
+}
